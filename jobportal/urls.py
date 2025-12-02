592977c9ae63e62c14f9ac2461c49fb17f9fad0e
Anand Shukla <145691410+Anand9450@users.noreply.github.com>
2025-12-03 00:30:30 +0530

    Fix media files serving in production

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -59,4 +59,10 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+from django.views.static import serve 
+from django.urls import re_path
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
